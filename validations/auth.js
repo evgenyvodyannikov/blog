f711c64257215009f7cb0d2489a821d9e7b2e5fd
evgenyvodyannikov <66135471+evgenyvodyannikov@users.noreply.github.com>
2022-10-04 22:27:06 +0500

    validation update

--- validations/auth.js
+++ validations/auth.js
@@ -1,8 +1,8 @@
 import { body } from 'express-validator'
 
-export const registerValidator = [
-    body('email').isEmail(),
-    body('password').isLength({min: 8}),
-    body('fullName').isLength({min: 2}),
-    body('avatarURL').optional().isURL(),
+export const registerValidation = [
+    body('email', 'Неверный формат почты').isEmail(),
+    body('password', 'Пароль не может содержать менее 8 символов').isLength({min: 8}),
+    body('fullName', 'Укажите имя').isLength({min: 2}),
+    body('avatarURL', 'Неверная ссылка на фото').optional().isURL(),
 ];
