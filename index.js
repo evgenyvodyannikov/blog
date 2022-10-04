ab2fe27c4fe4f80049ba7cbb2477b7ca7f1764bf
evgenyvodyannikov <66135471+evgenyvodyannikov@users.noreply.github.com>
2022-10-04 22:47:02 +0500

    encrypt password

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-import express from 'express';
+import express from 'express'
 import jwt from 'jsonwebtoken'
-import mongoose from 'mongoose';
+import bcrypt from 'bcrypt'
+import mongoose from 'mongoose'
 import { validationResult } from 'express-validator'
 
 import { registerValidation } from './validations/auth.js'
 
-mongoose.connect('mongodb+srv://admin:rIbplCZpPavfbwkA@cluster0.1cogdfr.mongodb.net/?retryWrites=true&w=majority')
-.then(() => console.log('DB OK'))
-.catch((err) => console.log('DB error:', err));
-const app =  express();
+import userModel from './models/user.js'
+
+mongoose.connect('mongodb+srv://admin:rIbplCZpPavfbwkA@cluster0.1cogdfr.mongodb.net/blog?retryWrites=true&w=majority')
+    .then(() => console.log('DB OK'))
+    .catch((err) => console.log('DB error:', err));
+const app = express();
 
 app.use(express.json());
 
@@ -27,22 +30,40 @@ app.post('/auth/login', (req, res) => {
 
 });
 
-app.post('/auth/register', registerValidation, (req, res) => {
+app.post('/auth/register', registerValidation, async (req, res) => {
 
-  const errors = validationResult(req);
-  if(!errors.isEmpty()){
-    return res.status(400).json(errors.array());
-  }
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json(errors.array());
+        }
 
-  res.json({
-    success: true,
-  });
+        const password = req.body.password;
+        const salt = await bcrypt.genSalt(10);
+        const passwordHash = await bcrypt.hash(password, salt);
 
+        const doc = new userModel({
+            email: req.body.email,
+            fullName: req.body.fullName,
+            passwordHash,
+            avatarURL: req.body.avatarURL
+        });
+
+        const user = await doc.save();
+
+        res.json(user);
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: "Ошибка регистрации"
+        })
+    }
 
 });
 
 app.listen(4444, (err) => {
-    if(err){
+    if (err) {
         return console.log("Server error: ", err);
     }
     console.log("Server OK");
