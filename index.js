b187344de25b90006e08b8177e2d9e66282cb0fd
eugenevodyannikov <vodyannikov.eugene@gmail.com>
2022-10-04 11:44:42 +0500

    auth structure added

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 import express from 'express';
-
+import jwt from 'jsonwebtoken'
 const app =  express();
 
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello world!');
+})
+
+app.post('/login', (req, res) => {
+
+    const token = jwt.sign({
+        email: req.body.email,
+        fullName: "Some Name",
+    }, 'secretKey')
+
+    res.json({
+        success: true,
+        token
+    });
+
 })
 
 app.listen(4444, (err) => {
