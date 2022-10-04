c79b13adb20592b9dd607557435433b7e7c27ddc
eugenevodyannikov <vodyannikov.eugene@gmail.com>
2022-10-04 12:01:42 +0500

    configure DB connection

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import express from 'express';
 import jwt from 'jsonwebtoken'
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb+srv://admin:rIbplCZpPavfbwkA@cluster0.1cogdfr.mongodb.net/?retryWrites=true&w=majority')
+.then(() => console.log('DB OK'))
+.catch((err) => console.log('DB error:', err));
 const app =  express();
 
 app.use(express.json());
