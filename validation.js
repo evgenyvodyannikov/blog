2f8fa611804304959ed7dba2a25cd85452065f1d
eugenevodyannikov <vodyannikov.eugene@gmail.com>
2022-10-07 11:27:24 +0500

    post create validation

--- validation.js
+++ validation.js
@@ -11,3 +11,10 @@ export const loginValidation = [
     body('email', 'Неверный формат почты').isEmail(),
     body('password', 'Пароль не может содержать менее 8 символов').isLength({min: 8})
 ];
+
+export const postCreateValidation = [
+    body('title', 'Введите заголовок статьи').isLength({min: 3}).isString(),
+    body('text', 'Введите содержание статьи').isLength({min: 10}).isString(),
+    body('tags', 'Неверный формат тегов').optional().isString(),
+    body('imageUrl', 'Неверная ссылка на изображение').optional().isString(),
+];
